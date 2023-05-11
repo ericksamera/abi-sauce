9e6ace46aa002a70e2ea7f612982c19f3b926276
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2023-05-11 12:26:01 -0700

    Update streamlit-sanger-sequence-trim.py

--- streamlit-sanger-sequence-trim.py
+++ streamlit-sanger-sequence-trim.py
@@ -50,20 +50,57 @@ class App:
                 st.divider()
                 with st.expander('**TRACE FILES:**', expanded=True):
 
-                    st.session_state.TRACE_LIST_SORT = st.radio(
-                        'sorting method',
-                        ['Sequence ID', 'Well Position'],
-                        horizontal=True)
+                    st.session_state.FLAGS = st.multiselect(
+                        'Select property to display with name.',
+                        [
+                            'Qualitative Score',
+                            'Trace Score',
+                            'PUP Score',
+                            'Contiguous Read Length (CRL)',
+                            'Ambiguous Nucleotide Count'
+                            ],
+                        ['Qualitative Score'],
+                    )
+
+
 
                     _translate_labels = {
                         'Sequence ID': 'name',
-                        'Well Position': 'well'
+                        'Well Position': 'well',
+                        'Trace Score': 'trace_score',
+                        'PUP Score': 'pup_score',
+                        'Contiguous Read Length (CRL)': 'crl_score',
+                        'Ambiguous Nucleotide Count' : 'ambig_count',
                     }
-                    
+
+                    st.session_state.REVERSE_SORT = st.checkbox(
+                        'Reverse sort'
+                    )
+
+                    st.session_state.TRACE_LIST_SORT = st.selectbox(
+                        'Sort by:',
+                        list(_translate_labels.keys()))
+
+                    def show_flags_with_name(_seq_name, _flags_list) -> str:
+
+                        _flags_dict = {
+                            'Qualitative Score': st.session_state.PROCESSED_FILES[f"{_seq_name}.ab1"]['color_code'],
+                            'Trace Score': str(st.session_state.PROCESSED_FILES[f"{_seq_name}.ab1"]['trace_score']).zfill(2),
+                            'PUP Score': str(st.session_state.PROCESSED_FILES[f"{_seq_name}.ab1"]['pup_score']).zfill(2),
+                            'Contiguous Read Length (CRL)': str(st.session_state.PROCESSED_FILES[f"{_seq_name}.ab1"]['crl_score']).zfill(2),
+                            'Ambiguous Nucleotide Count': str(st.session_state.PROCESSED_FILES[f"{_seq_name}.ab1"]['ambig_count']).zfill(2),
+                        }
+
+                        flags_and_name = '|'.join([str(_flags_dict[i]) for i in st.session_state.FLAGS] + ['']) + st.session_state.PROCESSED_FILES[f'{_seq_name}.ab1']['name']
+
+                        return flags_and_name
+
+                    name_func = lambda x: st.session_state.PROCESSED_FILES[f"{x}.ab1"]['color_code'] + st.session_state.PROCESSED_FILES[f'{x}.ab1']['name']
+                
                     st.session_state.SELECTED_TRACE = st.radio(
                         'Select trace file to view:', 
-                        options=[seq_object['name'] for seq_object in sorted(st.session_state.PROCESSED_FILES.values(), key=lambda x: x[_translate_labels[st.session_state.TRACE_LIST_SORT]])],
-                        format_func=lambda x: st.session_state.PROCESSED_FILES[f"{x}.ab1"]['color_code'] + st.session_state.PROCESSED_FILES[f'{x}.ab1']['name'])
+                        options=[seq_object['name'] for seq_object in sorted(st.session_state.PROCESSED_FILES.values(), reverse=st.session_state.REVERSE_SORT, key=lambda x: x[_translate_labels[st.session_state.TRACE_LIST_SORT]])],
+                        format_func=lambda x: show_flags_with_name(x, st.session_state.FLAGS))
                 with st.expander('**DOWNLOAD OPTIONS:**'):
                     st.session_state.DEFAULT_FILENAME = 'abi-sauce-trim'
                     st.session_state.USER_FILENAME = st.text_input('File name', placeholder='abi-sauce-trim')
@@ -141,8 +178,9 @@ class App:
                 'pup_score': seq_object_raw.annotations['abif_raw']['PuSc1'] if 'PuSc1' in seq_object_raw.annotations['abif_raw'] else -1,
                 'trace_score': seq_object_raw.annotations['abif_raw']['TrSc1'] if 'TrSc1' in seq_object_raw.annotations['abif_raw'] else -1,
                 'crl_score': seq_object_raw.annotations['abif_raw']['CRLn1'] if 'CRLn1' in seq_object_raw.annotations['abif_raw'] else -1,
-                'left_trim': seq_object_raw.seq.find(seq_object_trimmed.seq[0:5])-1,
+                'left_trim': seq_object_raw.seq.find(seq_object_trimmed.seq[0:5]),
                 'right_trim': len(seq_object_raw.seq) - len(seq_object_trimmed) - seq_object_raw.seq.find(seq_object_trimmed.seq[0:5])-1,
+                'ambig_count': sum([seq_object_trimmed.seq.count(nuc) for nuc in 'NRYKMSWBDHV']) if st.session_state.TRIM else sum([seq_object_raw.seq.count(nuc) for nuc in 'NRYKMSWBDHV'])
                 }
             seq_object_dict['scoring_str'] = f" ({seq_object_dict['pup_score']}/{seq_object_dict['trace_score']}/{seq_object_dict['crl_score']}) "
             if all([
@@ -183,8 +221,9 @@ class App:
         """
         raw_annotations = _seq_object_dict['_raw'].annotations['abif_raw']
         #trim_annotations = seq_object_dict['_trimmed'].annotations['abif_raw']
-        left_trim = raw_annotations['PLOC2'][_seq_object_dict['left_trim']+1] #
-        right_trim = raw_annotations['PLOC2'][-_seq_object_dict['right_trim']-2] #+ raw_annotations['SPAC3']/2
+        left_trim = raw_annotations['PLOC2'][_seq_object_dict['left_trim']] #
+        try: right_trim = raw_annotations['PLOC2'][-_seq_object_dict['right_trim']-2] + raw_annotations['SPAC3']/2
+        except IndexError: right_trim = raw_annotations['PLOC2'][-_seq_object_dict['right_trim']] + raw_annotations['SPAC3']/2
         
         phred_scores = _seq_object_dict['_raw'].letter_annotations['phred_quality']
         nucleotide_plots = {
@@ -278,9 +317,32 @@ class App:
         st.plotly_chart(
             self._plot_electropherogram(st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]),
             use_container_width=True)
-        help_str = "Trimmed with Mott's trimming algorithm!" if st.session_state.TRIM_STR else "Untrimmed!"
+        help_str = "Trimmed with Mott's trimming algorithm!" if st.session_state.TRIM_STR == '_trimmed' else "Untrimmed!"
         st.caption(f'FASTA sequence: ({len(st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"][st.session_state.TRIM_STR])} bp)', help=help_str)
         st.code(st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"][st.session_state.TRIM_STR].format("fasta"))
+        if st.session_state.TRIM_STR == '_trimmed':
+            with st.expander(f'Untrimmed FASTA ({len(st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]["_raw"])} bp):'):
+                st.code(st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]["_raw"].format("fasta"))
+            st.subheader('Trimming statistics')
+            left_column, right_column = st.columns(2)
+            with left_column:
+                l_trim_count: int = st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]["left_trim"]
+                r_trim_count: int = st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]["right_trim"]+1
+                total_trim_count = l_trim_count+r_trim_count
+
+                phred_scores_raw = st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]['_raw'].letter_annotations['phred_quality']
+                phred_scores_trimmed = st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]['_trimmed'].letter_annotations['phred_quality']
+
+                average_phred_raw = sum(phred_scores_raw)/len(phred_scores_raw)
+                average_phred_trimmed = sum(phred_scores_trimmed)/len(phred_scores_trimmed)
+
+                st.markdown(f'Left trim: `{l_trim_count}` | Right trim:`{r_trim_count}` (Total: `{total_trim_count}`)', help='The right side is usually worse.')
+                st.markdown(f'Average PHRED score (before): `{average_phred_raw:.1f}` | (after) `{average_phred_trimmed:.1f}`')
+
+            with right_column:
+                """"""
+                #st.markdown(f'Right trim,: {st.session_state.PROCESSED_FILES[f"{st.session_state.SELECTED_TRACE}.ab1"]["right_trim"]+1} nucleotides')
+
 # --------------------------------------------------
 if __name__=="__main__":
     streamlit_app = App()
