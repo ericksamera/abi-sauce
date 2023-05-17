c9785c79f06f0b056e4300fbf28c89ab92151b0d
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2023-05-17 04:17:44 -0700

    BREAKING!: individual plots, genotyping table

--- pages/2_🌈_fla-viewer.py
+++ pages/2_🌈_fla-viewer.py
@@ -4,7 +4,7 @@ __description__ =\
 Purpose: Streamlit wrapper for sanger-sequence-trim.
 """
 __author__ = "Erick Samera"
-__version__ = "1.1.0"
+__version__ = "1.5.0"
 __comments__ = "stable enough"
 # --------------------------------------------------
 import streamlit as st
@@ -103,8 +103,8 @@ class App:
                 st.error('Select some files!')
     def _upload_files(self, _st_uploaded_files: list) -> None:
         if not _st_uploaded_files: return None
-        st.session_state.UPLOADED_FLA = _st_uploaded_files
-        st.session_state.PROCESSED_FLA = self._process_files(_st_uploaded_files)
+        st.session_state.UPLOADED_FLA = [file for file in _st_uploaded_files if 'QCReport' not in file.name]
+        st.session_state.PROCESSED_FLA = self._process_files(st.session_state.UPLOADED_FLA)
     def _process_files(self, _st_uploaded_files) -> None:
         """
         Function creates a new instance of the copied file and processes it.
@@ -126,6 +126,7 @@ class App:
                 if channel_color not in processed_files[processed_name]['channels']: 
                     processed_files[processed_name]['channels'][channel_color] = {
                         'name': channel_color,
+                        'color_name': channel_color,
                         'x': [],
                         'y': []
                     }
@@ -151,17 +152,49 @@ class App:
                 processed_files[processed_name]['channels'][channel_color]['x'] += [None,start,middle,end,None]
                 processed_files[processed_name]['channels'][channel_color]['y'] += [None,0,height,0,None]
 
-                processed_files[processed_name]['peaks'][channel_color]['x'] += [None,middle,None]
-                processed_files[processed_name]['peaks'][channel_color]['y'] += [None,height,None]
+                processed_files[processed_name]['peaks'][channel_color]['x'] += [middle]
+                processed_files[processed_name]['peaks'][channel_color]['y'] += [height]
+            processed_files[processed_name]['predicted_genotypes'] = self._predict_genotypes(processed_files[processed_name])
         return processed_files
+    def _predict_genotypes(self, _processed_file_dict: dict) -> None:
+        """
+        """
+        predicted_genotypes_dict = {}
+        for color in _processed_file_dict['peaks']:
+            if color in ('orange'): continue
+            if color not in predicted_genotypes_dict: predicted_genotypes_dict[color] = []
+            try: max_height = max([y_value for x_value, y_value in zip(_processed_file_dict['peaks'][color]['x'], _processed_file_dict['peaks'][color]['y']) if x_value>50])
+            except ValueError: continue
+            for x_value, y_value in zip(_processed_file_dict['peaks'][color]['x'], _processed_file_dict['peaks'][color]['y']):
+                if x_value<50: continue
+                if y_value > max_height/5:
+                    predicted_genotypes_dict[color].append(
+                        {'channel': color.upper(), 'position (bp)': f"{x_value:.1f}", 'height': f"{y_value:.0f}"})
+        return predicted_genotypes_dict
     def _plot_total_trace(self, _trace_dict):
         """
         """
 
         fig = go.Figure()
+
+        all_x_vals = []
+        for x_vals in _trace_dict['channels'].values():
+            all_x_vals += [x for x in x_vals['x'] if x]
+        max_x_val = max(all_x_vals)
+
+        maxes_no_ladder = []
+        for color in [key for key in _trace_dict['channels'].keys() if key not in ['orange']]:
+            try:
+                list_to_add = max([i for i in [y_vals if x_vals>50 else None for x_vals, y_vals in zip(_trace_dict['channels'][color]['x'], _trace_dict['channels'][color]['y']) if x_vals] if i])
+                maxes_no_ladder.append(list_to_add)
+            except:continue
+        max_ladder = max([i for i in _trace_dict['channels']['orange']['y'] if i])
+        average_height = max(maxes_no_ladder) if max(maxes_no_ladder)>max_ladder else max_ladder
+
         for color, values in _trace_dict['channels'].items():
             fig.add_trace(
                 go.Scatter(
+                    mode='lines',
                     x=values['x'],
                     y=values['y'],
                     hoverinfo='skip',
@@ -174,6 +207,7 @@ class App:
                 ))
             fig.add_trace(
                 go.Scatter(
+                    mode='markers',
                     x=_trace_dict['peaks'][color]['x'],
                     y=_trace_dict['peaks'][color]['y'],
                     #hoverinfo='skip',
@@ -187,22 +221,74 @@ class App:
 
         fig.update_layout(
             legend_title_text='Channels',
-            xaxis=dict(rangeslider=dict(visible=True, thickness=0.1), constrain='domain')
+            xaxis=dict(
+                range=(0, max_x_val+20),
+                rangeslider=dict(visible=True, thickness=0.1, range=(0, max_x_val+20)), constrain='domain'),
+            yaxis=dict(range=(0, average_height))
             )
         return fig
+    def _per_channel_bar_plot(self, _per_channel_trace_dict, _per_channel_peaks_dict):
+        """
+        """
 
+        averages_past_50 = []
+        for x_val, y_val in zip(_per_channel_peaks_dict['x'], _per_channel_peaks_dict['y']):
+            if x_val<50:continue
+            averages_past_50.append(y_val)
+        average_height = max(averages_past_50)
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(
+            mode='lines',
+            x=_per_channel_trace_dict['x'],
+            y=_per_channel_trace_dict['y'],
+            name=_per_channel_trace_dict['color_name'].upper(),
+            hoverinfo='skip',
+            fill='tozeroy',
+            marker=dict(
+                color=_per_channel_trace_dict['color_name'].lower() if _per_channel_trace_dict['color_name'].lower() not in ['yellow'] else 'black'
+                ),
+            )
+        )
+        fig.add_trace(go.Scatter(
+            mode='markers',
+            x=_per_channel_peaks_dict['x'],
+            y=_per_channel_peaks_dict['y'],
+            #hoverinfo='skip',
+            marker=dict(
+                size=0.5,
+                color=_per_channel_trace_dict['color_name'].lower() if _per_channel_trace_dict['color_name'].lower() not in ['yellow'] else 'black'),
+            name=_per_channel_trace_dict['color_name'].upper(),
+            showlegend=False,
+            ))
+
+        fig.update_layout(
+            margin=dict(l=50, r=150, t=0, b=0),
+            height=100,
+            legend_title_text='Channels',
+            #xaxis=dict(rangeslider=dict(visible=True, thickness=0.1, range=(0,max([val for val in _trace_dict['peaks'][color]['x'] if val]))), constrain='domain'),
+            xaxis=dict(range=(0, max([val for val in _per_channel_trace_dict['x'] if val])+50)),
+            yaxis=dict(range=(0, average_height)),
+            modebar=dict(remove=[ "autoScale2d", "autoscale", "editInChartStudio", "editinchartstudio", "hoverCompareCartesian", "hovercompare", "lasso", "lasso2d", "orbitRotation", "orbitrotation", "pan", "pan2d", "pan3d", "reset", "resetCameraDefault3d", "resetCameraLastSave3d", "resetGeo", "resetSankeyGroup", "resetScale2d", "resetViewMapbox", "resetViews", "resetcameradefault", "resetcameralastsave", "resetsankeygroup", "resetscale", "resetview", "resetviews", "select", "select2d", "sendDataToCloud", "senddatatocloud", "tableRotation", "tablerotation", "toImage", "toggleHover", "toggleSpikelines", "togglehover", "togglespikelines", "toimage", "zoom", "zoom2d", "zoom3d", "zoomIn2d", "zoomInGeo", "zoomInMapbox", "zoomOut2d", "zoomOutGeo", "zoomOutMapbox", "zoomin", "zoomout"]),
+            )
+        return fig
     def _plot_per_channel_traces(self, _trace_dict):
         """
         """
-        fig = go.Figure()
-        for color in sorted([key for key in _trace_dict['channels'].keys() if key.lower() not in 'orange']) + ['orange']:
-            new_trace_dict = {
-                'channels': {colorval: _trace_dict['channels'][colorval] for colorval in [color]},
-                'peaks': {colorval: _trace_dict['peaks'][colorval] for colorval in [color]}
-                }
-            st.plotly_chart(
-                self._plot_total_trace(new_trace_dict),
-                use_container_width=True)
+        with st.expander('Individual channels'):
+            for color, values in [(color, value) for color, value in _trace_dict['channels'].items() if color not in ['orange']] + [('orange', _trace_dict['channels']['orange'])]:
+                if not any([y_val if x_val>50 else None for x_val, y_val in zip(values['x'], values['y']) if x_val]): continue
+
+                st.plotly_chart(
+                    self._per_channel_bar_plot(_trace_dict['channels'][color], _trace_dict['peaks'][color]),
+                    use_container_width=True)
+                if color not in ('orange'): st.dataframe(_trace_dict['predicted_genotypes'][color], use_container_width=True)
+        
+        with st.expander('Full predicted genotypes'):
+            full_genotype_table = []
+            for color in _trace_dict['predicted_genotypes']:
+                full_genotype_table += _trace_dict['predicted_genotypes'][color]
+            st.dataframe(sorted(full_genotype_table, key=lambda x: float(x['position (bp)']), reverse=False), use_container_width=True)
     def _update_plot_window(self):
         """
         """
@@ -211,9 +297,7 @@ class App:
         st.plotly_chart(
             self._plot_total_trace(st.session_state.PROCESSED_FLA[f"{st.session_state.SELECTED_TRACE}"]),
             use_container_width=True)
-        
-        with st.expander('Individual traces'):
-            self._plot_per_channel_traces(st.session_state.PROCESSED_FLA[f"{st.session_state.SELECTED_TRACE}"])
+        self._plot_per_channel_traces(st.session_state.PROCESSED_FLA[f"{st.session_state.SELECTED_TRACE}"])
     #     help_str = "Trimmed with Mott's trimming algorithm!" if st.session_state.TRIM_STR == '_trimmed' else "Untrimmed!"
     #     st.caption(f'FASTA sequence: ({len(st.session_state.PROCESSED_FLA[f"{st.session_state.SELECTED_TRACE}.ab1"][st.session_state.TRIM_STR])} bp)', help=help_str)
     #     st.code(st.session_state.PROCESSED_FLA[f"{st.session_state.SELECTED_TRACE}.ab1"][st.session_state.TRIM_STR].format("fasta"))
