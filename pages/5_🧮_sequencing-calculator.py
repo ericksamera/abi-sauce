18e4d9498563f5a1a524566bca05fb43f8e7a227
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2024-05-08 07:14:19 -0700

    FIX: aesthetics + 1kb issue

--- pages/5_🧮_sequencing-calculator.py
+++ pages/5_🧮_sequencing-calculator.py
@@ -9,7 +9,6 @@ __date__ = "20240507"
 __comments__ = "minimally functional"
 # --------------------------------------------------
 import streamlit as st
-import streamlit_ext as ste
 # --------------------------------------------------
 class App:
     def __init__(self):
@@ -43,7 +42,7 @@ class App:
         elif input_str.lower().endswith('b'): input_str = input_str.lower().split('b')[0]
         else: input_str = input_str
 
-        if input_str.upper()[-1] in 'MBG':
+        if input_str.upper()[-1] in 'GMK':
             conversion_factor = {
                 'G': 1_000_000_000,
                 'M': 1_000_000,
@@ -60,11 +59,11 @@ class App:
 
         #x_samples_per, x_depth = st.tabs(["Samples per flow cell", "Dog",])
         output_per_unit_dict = {
-            "MiSeq v3 (2x300): 15000000000": 15000000000,
-            "MiSeq v2 (2x250): 15000000000": 15000000000,
-            "MiSeq v2 Nano (2x250): 600000000": 600000000,
-            "Miseq v2 Micro (2x150): 2400000000": 2400000000,
-            "MinION: 15000000000": 15000000000
+            "MiSeq v3 (2x300)": 15000000000,
+            "MiSeq v2 (2x250)": 15000000000,
+            "MiSeq v2 Nano (2x250)": 600000000,
+            "Miseq v2 Micro (2x150)": 2400000000,
+            "MinION, FLO-MIN114": 15000000000
         }
 
         variable_of_interest_list = [
@@ -87,42 +86,74 @@ class App:
         depth = st.number_input("Sequencing Depth (X)", value=20, disabled=True if variable_of_interest=="depth" else False)
         dup_col1, dup_col2 = st.columns(2)
         with dup_col1:
-            duplication = st.number_input("Percent Read Duplication (%)", value=3, min_value=0, max_value=100)
+            duplication = st.number_input("Percent Read Duplication (%)", value=float(2.5), min_value=float(0), max_value=float(99.9), step=float(1.0), format="%f")
         with dup_col2:
             st.caption("Duplications occur when multiple copies of the same original molecule are sequenced. The frequency of duplication is protocol dependent.")
-            st.caption("The default (2%) is reasonable, according to experimental data.")
+            st.caption("The default (2.5%) is reasonable for Illumina sequencing, according to experimental data.")
         on_target = st.number_input("Percent Reads On-target (%)", value=85, min_value=0, max_value=100)
 
-        output_per_unit = st.selectbox(
-            "Sequencing Output (bp)",
-            options=(
-            "MiSeq v3 (2x300): 15000000000",
-            "MiSeq v2 (2x250): 15000000000",
-            "MiSeq v2 Nano (2x250): 600000000",
-            "Miseq v2 Micro (2x150): 2400000000",
-            "MinION: 15000000000"),
-            index=0,
-            placeholder="Select sequencing chemistry",)
+        seq_out_col1, seq_out_col2 = st.columns(2)
+        with seq_out_col1:
+            output_per_unit = st.selectbox(
+                "Sequencing Output (bp)",
+                options=(
+                    "MiSeq v3 (2x300)",
+                    "MiSeq v2 (2x250)",
+                    "MiSeq v2 Nano (2x250)",
+                    "Miseq v2 Micro (2x150)",
+                    "MinION, FLO-MIN114"),
+                index=0,
+                placeholder="Select sequencing chemistry")
         
+        unit_output = output_per_unit_dict[output_per_unit]
+        with seq_out_col2:
+            if output_per_unit == "MinION, FLO-MIN114":
+                
+                exp_runtime = st.slider(
+                    "Runtime (hrs)",
+                    value=72,
+                    min_value=0,
+                    max_value=72,
+                    step=1)
+                
+                model_option = st.selectbox(
+                    "Model",
+                    ("Worst so far", "Probable", "Optimistic") if exp_runtime != 72 else ("Worst so far", "Worst WGS", "Probable", "Optimistic", "Theoretical"),
+                    help="Base output is calculated by a third-order polynomial model from experimental data.",)
+
+                model_option_to_eq = {
+                    'Optimistic': lambda x: (-114458843.02911) + (7555412.38270*x) + (-279.48619*(x**2)) + (-0.09880*(x**3)),
+                    'Probable': lambda x: 0.8 * ((-114458843.02911) + (7555412.38270*x) + (-279.48619*(x**2)) + (-0.09880*(x**3))),
+                    'Worst WGS': lambda x: 0.85 * ((-198480166.79748) + (6999111.75559*x) + (-1270.41713*(x**2)) + (0.08046*(x**3))),
+                    'Worst so far': lambda x: (-240161649.78617) + (6337746.57404*x) + (-2252.15270*(x**2)) + (0.26408*(x**3)),
+                    'Theoretical': lambda x: 3472222.22222 * x,
+                }
+
+                unit_output = model_option_to_eq[model_option](exp_runtime * 60)
+                st.caption(f"Sequencing output: {unit_output / 1_000_000_000:.1f} Gbp with a runtime of {exp_runtime} hrs.")
+
         if variable_of_interest != "depth":
             total_output_required = region_size_int * depth / ((1-(duplication/100)) * on_target/100)
 
         if variable_of_interest == variable_of_interest_list[0]: 
-                output = output_per_unit_dict[output_per_unit] / total_output_required
+                output = unit_output / total_output_required
                 output_unit = "samples per flow cell."
         elif variable_of_interest == variable_of_interest_list[1]:
-                output = ((output_per_unit_dict[output_per_unit]/samples_per_unit) * ((1-(duplication/100)) * on_target/100) / region_size_int)
+                output = ((unit_output/samples_per_unit) * ((1-(duplication/100)) * on_target/100) / region_size_int)
                 output_unit = f"X sequencing depth across {samples_per_unit} samples."
         elif variable_of_interest == variable_of_interest_list[2]:
-                reverse_conversion = st.radio("Unit", ["bp", "Kbp", "Mbp", "Gbp"])
-                reverse_conversion_dict = {
-                    'bp': 1,
-                    'Kbp': 1_000,
-                    'Mbp': 1_000_000,
-                    'Gbp': 1_000_000_000
-                }
-                output = ((output_per_unit_dict[output_per_unit]/samples_per_unit) * ((1-(duplication/100)) * (on_target/100) / depth)) / reverse_conversion_dict[reverse_conversion]
-                output_unit = f"{reverse_conversion} region/genome assuming {depth} depth across {samples_per_unit} samples."
+                region_size_col1, region_size_col2 = st.columns(2)
+                with region_size_col2:
+                    reverse_conversion = st.radio("Unit", ["bp", "Kbp", "Mbp", "Gbp"])
+                    reverse_conversion_dict = {
+                        'bp': 1,
+                        'Kbp': 1_000,
+                        'Mbp': 1_000_000,
+                        'Gbp': 1_000_000_000
+                    }
+                with region_size_col1:
+                    output = ((unit_output/samples_per_unit) * ((1-(duplication/100)) * (on_target/100) / depth)) / reverse_conversion_dict[reverse_conversion]
+                    output_unit = f"{reverse_conversion} region/genome assuming {depth} depth across {samples_per_unit} samples."
         else:
                 output = 0
                 output_unit = ""
